9df8d58ea05a8bf5295f0a19a73a863e9fcbedd1
Daxesh-Italiya <daxeshitaliya58@gmail.com>
2022-09-15 20:24:01 +0530

    Update : tableView

--- src/components/CallsAndPuts2/MoreInfo.js
+++ src/components/CallsAndPuts2/MoreInfo.js
@@ -16,51 +16,48 @@ function MoreInfo({ showMoreDetails, handleCloseModal }) {
       open={showMoreDetails.show}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
-      style={{ overflow: "scroll", height: "80vh" }}
     >
-      <div>
-        <Box sx={{ ...style, width: 800 }}>
-          <div class="text-end" style={{ cursor: "pointer" }}>
-            <i class="fas fa-times" onClick={handleCloseModal}></i>
-          </div>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {`${showMoreDetails.data["key"]}`}
-          </Typography>
-          <div class="table-responsive shadow-lg p-3 my-3">
-            <table class="table">
-              <thead>
-                <tr>
-                  <td scope="col">Expiration Date</td>
-                  <td scope="col">Call IR</td>
-                  <td scope="col">Put IR</td>
-                  <td scope="col">CP Ratio</td>
-                  <td scope="col">HVTF</td>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.entries(showMoreDetails.data["value"]).map(
-                  ([key, value]) => {
-                    return (
-                      <tr>
-                        <td>{key}</td>
-                        <td>{value[0].toFixed(2)}</td>
-                        <td>{value[1].toFixed(2)}</td>
-                        <td>{value[2].toFixed(2)}</td>
-                        <td>{value[3].toFixed(2)}</td>
-                      </tr>
-                    );
-                  }
-                )}
-              </tbody>
-            </table>
-          </div>
-          <div>
-            <button onClick={handleCloseModal} class="btn btn-danger">
-              Close
-            </button>
-          </div>
-        </Box>
-      </div>
+      <Box sx={{ ...style, width: 800, overflow: "scroll", height: "80vh" }}>
+        <div class="text-end" style={{ cursor: "pointer" }}>
+          <i class="fas fa-times" onClick={handleCloseModal}></i>
+        </div>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          {`${showMoreDetails.data["key"]}`}
+        </Typography>
+        <div class="table-responsive shadow-lg p-3 my-3">
+          <table class="table">
+            <thead>
+              <tr>
+                <td scope="col">Expiration Date</td>
+                <td scope="col">Call IR</td>
+                <td scope="col">Put IR</td>
+                <td scope="col">CP Ratio</td>
+                <td scope="col">HVTF</td>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(showMoreDetails.data["value"]).map(
+                ([key, value]) => {
+                  return (
+                    <tr>
+                      <td>{key}</td>
+                      <td>{value[0].toFixed(2)}</td>
+                      <td>{value[1].toFixed(2)}</td>
+                      <td>{value[2].toFixed(2)}</td>
+                      <td>{value[3].toFixed(2)}</td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <button onClick={handleCloseModal} class="btn btn-danger">
+            Close
+          </button>
+        </div>
+      </Box>
     </Modal>
   );
 }
